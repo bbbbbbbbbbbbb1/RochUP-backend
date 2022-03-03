0265c84d08a918f8c7f9ebc6342dda4ddffd3051
fairy26 <yoshida-yosei-vx@ynu.jp>
2022-03-03 09:13:26 +0000

    add: createMeetingの追加

--- db.go
+++ db.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
@@ -12,6 +13,19 @@ type User struct {
 	UserId       string //`json:"user_id`
 	UserName     string //`json:"user_name`
 	UserPassword string //`json:"user_password`
+}
+
+type Meeting struct {
+	MeetingId        int    `gorm:"AUTO_INCREMENT"`
+	MeetingName      string //`json:"meeting_name`
+	MeetingStartTime time.Time
+}
+
+type Participant struct {
+	MeetingId        int    //`json:"meeting_id`
+	UserId           string //`json:"user_id`
+	SpeakNum         int
+	ParticipantOrder int
 }
 
 // SQLConnect DB接続
@@ -59,5 +73,34 @@ func loginUser(db *gorm.DB, userId string, userPassword string) bool {
 	} else {
 		fmt.Println("login失敗")
 		return false
+	}
+}
+
+func createMeeting(db *gorm.DB, meetingName string, startTimeStr string, presenters []string) (int, string, string, []string) {
+	var (
+		user         User
+		layout       = "2006/01/02 15:04:05"
+		startTime, _ = time.Parse(layout, startTimeStr)
+		meeting      = Meeting{MeetingName: meetingName, MeetingStartTime: startTime}
+	)
+
+	if err := db.Create(&meeting).Error; err == nil {
+		for i, presenter := range presenters {
+			if err := db.First(&user, "user_id = ?", presenter).Error; err == nil {
+				participant := Participant{MeetingId: meeting.MeetingId, UserId: user.UserId, SpeakNum: 0, ParticipantOrder: i}
+				if err := db.Create(&participant).Error; err != nil { // TODO: transaction
+					fmt.Printf("create失敗(発表者%sの登録に失敗しました): %s, %s, %s\n", presenter, meetingName, startTimeStr, presenters)
+					return -1, "", "", []string{}
+				}
+			} else {
+				fmt.Printf("create失敗(発表者%sが見つかりません): %s, %s, %s\n", presenter, meetingName, startTimeStr, presenters)
+				return -1, "", "", []string{}
+			}
+		}
+		fmt.Printf("create成功: %s, %s, %s\n", meetingName, startTimeStr, presenters)
+		return meeting.MeetingId, meetingName, startTimeStr, presenters
+	} else {
+		fmt.Printf("create失敗(会議の登録に失敗しました): %s, %s, %s\n", meetingName, startTimeStr, presenters)
+		return -1, "", "", []string{}
 	}
 }
